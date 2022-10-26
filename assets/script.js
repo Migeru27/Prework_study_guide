31e2e05921c2ecea185b347a861e1054f2bffaf2
Miguel A <migueldude27@gmail.com>
2022-10-26 17:18:54 -0400

    added js file with some minor edits

---
+++ assets/script.js
@@ -0,0 +1,44 @@
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+
+for (var x = 0; x < topics.length; x++) {
+ console.log(topics[x]);
+}
+
+/*
+
+//testing other stuff
+var hello, my, hal; 
+
+var test = [hello, my, hal]
+
+for(x = 0; x<=test.length;x++){
+
+    if(x == 0){
+        test[x] = "Hello my ";
+        document.write(test[x]);
+    }
+    else if(x == 1){
+        test[x] = "name is ";
+        document.write(test[x]);
+    }
+    else if(x == 2){
+        test[x] = "Haley.";
+        document.write(test[x]);
+    }
+
+
+    document.write(test);
+}
+
+if (topic === 'HTML') {
+ console.log("Let's study HTML!");
+} else if (topic === 'CSS') {
+ console.log("Let's study CSS!");
+} else if (topic === 'Git') {
+ console.log("Let's study Git!");
+} else if (topic === 'JavaScript') {
+ console.log("Let's study JavaScript!");
+} else {
+ console.log('Please try again!');
+}
+*/
